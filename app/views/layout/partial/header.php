4a6720b9d1dac314fcc2f2d79035bd1e65dffab8
NgynDonQun <ngyndonqun@gmail.com>
2026-06-12 14:08:10 +0700

    feat <sinhvien>: update, delete sinhvien

--- app/views/layout/partial/header.php
+++ app/views/layout/partial/header.php
@@ -6,31 +6,27 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo $title; ?></title>
   <style>
-    header nav {
-      height: 60px;
-    }
+
   </style>
 </head>
 
 <body>
   <!-- A grey horizontal navbar that becomes vertical on small screens -->
-  <header>
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-      <div class="container-fluid">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active" href="/sinhvien/index">Quản lý sinh viên</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/home/index">Quản lý lớp học</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <div class="container-fluid">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" href="/sinhvien/index">Quản lý sinh viên</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/home/index">Quản lý lớp học</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </body>
 
 </html>
